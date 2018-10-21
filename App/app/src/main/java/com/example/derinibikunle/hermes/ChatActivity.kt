ba94583a2fc0c2a0270a50f8be76df92f7408682
sebcarlucci <sebastiano.carlucci@gmail.com>
2018-10-21 16:30:39 -0700

    Added Chat Activity

---
+++ App/app/src/main/java/com/example/derinibikunle/hermes/ChatActivity.kt
@@ -0,0 +1,12 @@
+package com.example.derinibikunle.hermes
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ChatActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+    }
+}
