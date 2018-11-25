1c09a342dbce6da786e0356dc5d444362a548a88
allans21 <samallan22@gmail.com>
2018-11-24 17:19:01 -0800

    stuff

--- App/app/src/main/java/com/example/derinibikunle/hermes/DatabaseQuery.java
+++ App/app/src/main/java/com/example/derinibikunle/hermes/DatabaseQuery.java
@@ -1,5 +1,14 @@
 package com.example.derinibikunle.hermes;
 
+import android.support.annotation.NonNull;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -15,12 +24,33 @@ public class DatabaseQuery {
         return list;
     }
 
-    public static void pushToDb(EventObjects event) {
-        //do the firebase adding stuff
+    public static String getuid(){
+        return FirebaseAuth.getInstance().getCurrentUser().getUid();
     }
 
-    public static void removeFromDb(EventObjects event) {
-        //do the firebase removal stuff
+    public static void pushToDb(EventObjects event) {
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference().child("users");
+        mDatabase.child(getuid()).child("calendar_info").push().setValue(event);
+    }
+
+    public static void removeFromDb(final EventObjects event) {
+        final DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference().child("users").child(getuid()).child("calendar_info");
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot data : dataSnapshot.getChildren()){
+                    if(data.getValue(EventObjects.class) == event){
+                       String key =  data.getKey();
+                       mDatabase.child(key).removeValue();
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
 
