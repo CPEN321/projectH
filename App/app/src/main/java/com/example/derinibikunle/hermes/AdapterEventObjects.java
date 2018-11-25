5d406b62b0dfec442bc55444864adde355a34255
Amelia Casciola <amelia.casciola@gmail.com>
2018-11-25 14:58:58 -0800

    merged

---
+++ App/app/src/main/java/com/example/derinibikunle/hermes/AdapterEventObjects.java
@@ -0,0 +1,99 @@
+package com.example.derinibikunle.hermes;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import android.content.Context;
+import android.widget.Toast;
+
+public class AdapterEventObjects extends ArrayAdapter<EventObjects> {
+    private Activity activity;
+    private ArrayList<EventObjects> lEvents;
+    private static LayoutInflater inflater = null;
+    private Context context = this.getContext();
+
+    public AdapterEventObjects (Activity activity, int textViewResourceId, ArrayList<EventObjects> _lEvents) {
+        super(activity, textViewResourceId, _lEvents);
+        try {
+            this.activity = activity;
+            this.lEvents = _lEvents;
+
+            inflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        } catch (Exception e) {
+
+        }
+    }
+
+    public int getCount() {
+        return lEvents.size();
+    }
+
+//    public Product getItem(Product position) {
+//        return position;
+//    }
+
+    public long getItemId(int position) {
+        return position;
+    }
+
+    public static class ViewHolder {
+        public TextView display_title;
+        public TextView display_start_date;
+        public TextView display_end_date;
+        public Button delete_button;
+
+    }
+
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        View vi = convertView;
+        final ViewHolder holder;
+        try {
+            if (convertView == null) {
+                vi = inflater.inflate(R.layout.activity_event_objects, null);
+                holder = new ViewHolder();
+
+                holder.display_title = (TextView) vi.findViewById(R.id.event_title);
+                holder.display_start_date = (TextView) vi.findViewById(R.id.event_start_date);
+                holder.display_end_date = (TextView) vi.findViewById(R.id.event_end_date);
+                holder.delete_button = (Button) vi.findViewById(R.id.delete_button);
+
+                vi.setTag(holder);
+            } else {
+                holder = (ViewHolder) vi.getTag();
+            }
+
+            holder.display_title.setText(lEvents.get(position).getName());
+            holder.display_start_date.setText(lEvents.get(position).getStartDate().toString());
+            holder.display_end_date.setText(lEvents.get(position).getEndDate().toString());
+
+
+            holder.delete_button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    DatabaseQuery.removeFromDb(lEvents.get(position));
+                    lEvents.remove(position);
+                    Toast.makeText(context, "Event deleted!", Toast.LENGTH_LONG).show();
+
+                    Intent intent = new Intent(context, CustomCalendarActivity.class);
+                    context.startActivity(intent);
+                }
+            });
+
+
+        } catch (Exception e) {
+
+
+        }
+        return vi;
+    }
+}
---
+++ App/app/src/main/java/com/example/derinibikunle/hermes/ViewEventsActivity.java
@@ -0,0 +1,74 @@
+package com.example.derinibikunle.hermes;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+
+public class ViewEventsActivity extends AppCompatActivity {
+    private ListView eventList;
+    private Button doneButton;
+    private TextView eventsToday;
+    private Context context = this;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_events);
+
+        Bundle bundle = getIntent().getExtras();
+        int position = bundle.getInt("position");
+
+        eventList = (ListView)findViewById(R.id.list_of_events);
+        doneButton = (Button)findViewById(R.id.done_button);
+        eventsToday = (TextView)findViewById(R.id.events_today);
+        DatabaseQuery db = new DatabaseQuery();
+        Date clickDate = CustomCalendarActivity.dayValueInCells.get(position);
+        Toast.makeText(context, "Viewing events for: " +clickDate, Toast.LENGTH_LONG).show();
+
+        List<EventObjects> events = db.getAllFutureEvents();
+
+        //find events for today
+        for(EventObjects event:events) {
+            if((event.getStartDate().getDay() != clickDate.getDay()) ||
+                    (event.getStartDate().getMonth() != clickDate.getMonth()) ||
+                    (event.getStartDate().getYear() != clickDate.getYear()))
+                events.remove(event);
+        }
+
+        if(events.isEmpty()) {
+            //show that you have no events
+            eventsToday.setText("You have no events today.");
+        }
+        else {
+            eventsToday.setText("Your events today are: ");
+            AdapterEventObjects adapter;
+
+            adapter = new AdapterEventObjects(ViewEventsActivity.this, 0, (ArrayList<EventObjects>) events);
+            eventList.setAdapter(adapter);
+        }
+
+        doneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, CustomCalendarActivity.class);
+                context.startActivity(intent);
+            }
+        });
+    }
+}
