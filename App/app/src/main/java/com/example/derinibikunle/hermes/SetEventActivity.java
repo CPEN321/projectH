b11b9035e49520fc778a2eb20036c941038bb88d
allans21 <samallan22@gmail.com>
2018-11-25 21:20:50 -0800

    most of the calender works with the database

--- App/app/src/main/java/com/example/derinibikunle/hermes/SetEventActivity.java
+++ App/app/src/main/java/com/example/derinibikunle/hermes/SetEventActivity.java
@@ -148,7 +148,7 @@ public class SetEventActivity extends AppCompatActivity {
     public static Date getDate(int year, int month, int day, int hour, int minute) {
         Calendar cal = Calendar.getInstance();
         cal.set(Calendar.YEAR, year);
-        cal.set(Calendar.MONTH, month);
+        cal.set(Calendar.MONTH, month-1);
         cal.set(Calendar.DAY_OF_MONTH, day);
         cal.set(Calendar.HOUR_OF_DAY, hour);
         cal.set(Calendar.MINUTE, minute);
--- App/app/src/main/java/com/example/derinibikunle/hermes/ViewEventsActivity.java
+++ App/app/src/main/java/com/example/derinibikunle/hermes/ViewEventsActivity.java
@@ -2,6 +2,7 @@ package com.example.derinibikunle.hermes;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -13,10 +14,18 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+
+import static com.example.derinibikunle.hermes.DatabaseQuery.getuid;
 
 
 public class ViewEventsActivity extends AppCompatActivity {
@@ -38,30 +47,44 @@ public class ViewEventsActivity extends AppCompatActivity {
         doneButton = (Button)findViewById(R.id.done_button);
         eventsToday = (TextView)findViewById(R.id.events_today);
         DatabaseQuery db = new DatabaseQuery();
-        Date clickDate = CustomCalendarActivity.dayValueInCells.get(position);
+        final Date clickDate = CustomCalendarActivity.dayValueInCells.get(position);
         Toast.makeText(context, "Viewing events for: " +clickDate, Toast.LENGTH_LONG).show();
 
-        List<EventObjects> events = db.getAllFutureEvents();
+        //List<EventObjects> events = db.getAllFutureEvents();
 
         //find events for today
-        for(EventObjects event:events) {
-            if((event.getStartDate().getDay() != clickDate.getDay()) ||
-                    (event.getStartDate().getMonth() != clickDate.getMonth()) ||
-                    (event.getStartDate().getYear() != clickDate.getYear()))
-                events.remove(event);
-        }
 
-        if(events.isEmpty()) {
-            //show that you have no events
-            eventsToday.setText("You have no events today.");
-        }
-        else {
-            eventsToday.setText("Your events today are: ");
-            AdapterEventObjects adapter;
+        DatabaseReference mDataBase = FirebaseDatabase.getInstance().getReference().child("users").child(getuid()).child("calendar_info");
+        mDataBase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                List<EventObjects> list = new ArrayList<EventObjects>();
+                for(DataSnapshot data : dataSnapshot.getChildren()){
+                    list.add(data.getValue(EventObjects.class));
+                }
 
-            adapter = new AdapterEventObjects(ViewEventsActivity.this, 0, (ArrayList<EventObjects>) events);
-            eventList.setAdapter(adapter);
-        }
+                for(EventObjects event:list) {
+                    if((event.getStartDate().getDay() != clickDate.getDay()) ||
+                            (event.getStartDate().getMonth() != clickDate.getMonth()) ||
+                            (event.getStartDate().getYear() != clickDate.getYear()))
+                        list.remove(event);
+                }
+                if(list.isEmpty()) {
+                    //show that you have no events
+                    eventsToday.setText("You have no events today.");
+                }
+                else {
+                    eventsToday.setText("Your events today are: ");
+                    AdapterEventObjects adapter;
+
+                    adapter = new AdapterEventObjects(ViewEventsActivity.this, 0, (ArrayList<EventObjects>) list);
+                    eventList.setAdapter(adapter);
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
 
         doneButton.setOnClickListener(new View.OnClickListener() {
             @Override
